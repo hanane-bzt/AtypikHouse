22bd4a30d714a40a258954a35fd4966c9b711c45
benikiyounguila <benikiyounguila@etu-digitalschool.paris>
2024-06-01 11:53:48 +0200

    Modifications de Pays et Ville pour ajouter de nouvelles fonctionnalités

--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -3,48 +3,152 @@
 namespace App\Entity;
 
 use App\Repository\VilleRepository;
+use App\Validator\BanWord;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: VilleRepository::class)]
+#[UniqueEntity('name')]
+#[UniqueEntity('slug')]
+#[Vich\Uploadable()]
 class Ville
 {
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100)]
-    private ?string $nom = null;
+    #[ORM\Column(length: 255)]
+    #[Assert\Length(min: 10)]
+    private string $name = '';
 
     #[ORM\Column(length: 255)]
-    private ?string $pays = null;
+    #[Assert\Length(min: 5)]
+    #[Assert\Regex('/^[a-z0-9]+(?:-[a-z0-9]+)*$/', message: "Le slug ne peut contenir que des lettres minuscules, des chiffres et des tirets")]
+     private string $slug = '';
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(length: 255, nullable:true)]
+    private ?string $file = null;
+
+    #[Vich\UploadableField(mapping: 'villes', fileNameProperty: 'file' )]
+    #[Assert\Image()]
+    private ?File $thumbnailFile = null;
+
+
+    #[ORM\ManyToOne(inversedBy: 'villes', cascade: ['persist'])]
+    private ?Pays $pays = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getNom(): ?string
+    public function getName(): string
     {
-        return $this->nom;
+        return $this->name;
     }
 
-    public function setNom(string $nom): static
+    public function setName(string $name): static
     {
-        $this->nom = $nom;
+        $this->name = $name;
+
+        return $this;
+    }
+   
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
 
-    public function getPays(): ?string
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getSlug(): string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(?string $file): static
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    
+    public function getThumbnailFile(): ?File
+    {
+        return $this->thumbnailFile;
+    }
+
+    public function setThumbnailFile(?File $thumbnailFile): static
+    {
+        $this->thumbnailFile = $thumbnailFile;
+
+        return $this;
+    }
+
+    public function getPays(): ?Pays
     {
         return $this->pays;
     }
 
-    public function setPays(string $pays): static
+    public function setPays(?Pays $pays): static
     {
         $this->pays = $pays;
 
         return $this;
     }
+
+    // public function getAddress(): ?Address
+    // {
+    //     return $this->address;
+    // }
+
+    // public function setAddress(?Address $address): static
+    // {
+    //     $this->address = $address;
+
+    //     return $this;
+    // }
 }
